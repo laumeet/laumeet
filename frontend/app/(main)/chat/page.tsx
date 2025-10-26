7064852602dfc6a2485e7af11b89ff8eb209b027
HemmyEvo <141856365+HemmyEvo@users.noreply.github.com>
2025-10-26 23:11:26 +0100

    Update page.tsx

--- frontend/app/(main)/chat/page.tsx
+++ frontend/app/(main)/chat/page.tsx
@@ -547,7 +547,7 @@ export default function ChatPage() {
   }
 
   return (
-    <div className="h-screen bg-white dark:bg-gray-900 flex flex-col">
+    <div className="h-[100vh] pb-32 bg-white dark:bg-gray-900 flex flex-col">
       {/* Header */}
       <div className=" text-white px-4 py-4">
 
